56400a88e13d60715da44e88f8c3941fc5e9f366
Matt <9086152+MatthewFrench@users.noreply.github.com>
2023-10-21 19:36:57 -0500

    Added websocket tutorial code

--- PublicFacingServer/src/main.rs
+++ PublicFacingServer/src/main.rs
@@ -1,10 +1,31 @@
 mod generated_flatbuffers;
+
+use std::collections::HashMap;
 use generated_flatbuffers::monster_generated::my_game::sample::{
                                              Color, Equipment,
                                              Monster, MonsterArgs,
                                              Vec3};
 
-fn main() {
+use std::convert::Infallible;
+use std::sync::Arc;
+use tokio::sync::{mpsc, RwLock};
+use warp::{ws::Message, Filter, Rejection};
+
+mod handler;
+mod ws;
+
+type Result<T> = std::result::Result<T, Rejection>;
+type Clients = Arc<RwLock<HashMap<String, Client>>>;
+
+#[derive(Debug, Clone)]
+pub struct Client {
+    pub user_id: usize,
+    pub topics: Vec<String>,
+    pub sender: Option<mpsc::UnboundedSender<std::result::Result<Message, warp::Error>>>,
+}
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
 
     let mut builder = flatbuffers::FlatBufferBuilder::with_capacity(1024);
@@ -24,5 +45,46 @@ fn main() {
     });
     builder.finish(orc, None);
     let buf = builder.finished_data();
-    println!("Buffer size: {}", buf.len())
+    println!("Buffer size: {}", buf.len());
+
+    let clients: Clients = Arc::new(RwLock::new(HashMap::new()));
+
+    let health_route = warp::path!("health").and_then(handler::health_handler);
+
+    let register = warp::path("register");
+    let register_routes = register
+        .and(warp::post())
+        .and(warp::body::json())
+        .and(with_clients(clients.clone()))
+        .and_then(handler::register_handler)
+        .or(register
+            .and(warp::delete())
+            .and(warp::path::param())
+            .and(with_clients(clients.clone()))
+            .and_then(handler::unregister_handler));
+
+    let publish = warp::path!("publish")
+        .and(warp::body::json())
+        .and(with_clients(clients.clone()))
+        .and_then(handler::publish_handler);
+
+    let ws_route = warp::path("ws")
+        .and(warp::ws())
+        .and(warp::path::param())
+        .and(with_clients(clients.clone()))
+        .and_then(handler::ws_handler);
+
+    let routes = health_route
+        .or(register_routes)
+        .or(ws_route)
+        .or(publish)
+        .with(warp::cors().allow_any_origin());
+
+    println!("Running websocket server at 127.0.0.1:8000");
+
+    warp::serve(routes).run(([127, 0, 0, 1], 8000)).await;
+}
+
+fn with_clients(clients: Clients) -> impl Filter<Extract = (Clients,), Error = Infallible> + Clone {
+    warp::any().map(move || clients.clone())
 }
